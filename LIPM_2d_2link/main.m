92eec707f9f7d2a27260a00d675165a98b016898
michaelnaps <napoli.38@osu.edu>
2021-09-16 11:27:54 -0400

    added noteworthy figures to 2 link model

--- LIPM_2d_2link/main.m
+++ LIPM_2d_2link/main.m
@@ -13,16 +13,35 @@ clc;clear;
 close all;
 
 %% Task 2(a): simulation of the dynamics equation
-T  = 10;                % time span
+T  = 30;                % time span
 
 % initial variables, vertical rod
-th1_0 = [pi/2; 0];      % angle 1 - position and vel.
-th2_0 = [0; 0.01];      % angle 2 - position and vel.
+th1_0 = [pi/2; 0.01];      % angle 1 - position and vel.
+th2_0 = [00; 0];      % angle 2 - position and vel.
 th0 = [th1_0; th2_0];   % state space
 
 % solve nonlinear function using ode45
 [t, q] = ode45(@(t, q) statespace(q, 0), [0 T], th0); % u=0
 
+% plot values for theta 1 and 2
+% theta 1
+figure(1)
+hold on
+plot(t, q(:,1))
+plot(t, q(:,2))
+title('Theta1')
+legend('Angular Position', 'Angular Velocity')
+hold off
+
+% theta 2
+figure(2)
+hold on
+plot(t, q(:,3))
+plot(t, q(:,4))
+title('Theta2')
+legend('Angular Position', 'Angular Velocity')
+hold off
+
 % run animation for double-arm inverted pendulum
-q_plot = [q(:,1), q(:,3), q(:,2), q(:,4)];
-animation(q_plot', 0.01);
+% animation(q', 0.01);
+animation([q(:,1), q(:,3), q(:,2), q(:,4)]', 0.01);
