f2d882196445c5329c5a951736ed969af49e4aad
michaelnaps <napoli.38@osu.edu>
2021-09-15 23:54:35 -0400

    two link model needs adjustment, but animation is working properly

--- LIPM_2d_2link/main.m
+++ LIPM_2d_2link/main.m
@@ -17,11 +17,12 @@ T  = 10;                % time span
 
 % initial variables, vertical rod
 th1_0 = [pi/2; 0];      % angle 1 - position and vel.
-th2_0 = [pi/2; 0.01];   % angle 2 - position and vel.
-th0 = [th1_0; th2_0];         % state space
+th2_0 = [0; 0.01];      % angle 2 - position and vel.
+th0 = [th1_0; th2_0];   % state space
 
 % solve nonlinear function using ode45
 [t, q] = ode45(@(t, q) statespace(q, 0), [0 T], th0); % u=0
 
 % run animation for double-arm inverted pendulum
-animation(q', 0.01);
+q_plot = [q(:,1), q(:,3), q(:,2), q(:,4)];
+animation(q_plot', 0.01);
