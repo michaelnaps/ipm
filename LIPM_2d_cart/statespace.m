1d466494ef92d550eb7630ae0649962b6bfdb013
michaelnaps <napoli.38@osu.edu>
2021-10-03 20:02:51 -0400

    no change

--- LIPM_2d_cart/statespace.m
+++ LIPM_2d_cart/statespace.m
@@ -15,5 +15,5 @@ function [dq] = statespace(q, u, c)
     dq = [q2; 0; q4; 0];
     
     dq(2) = kp * u / (mc + mb);   
-    dq(4) = (q2 * q4 * sin(q3) - c * l * q4 - mb * l * g * sin(q3)) / ((mc + mb) * l);
+    dq(4) = (q2 * q4 * sin(q3) - c * l * q4 - mb * l * g * sin(q3)) / (mb * l);
 end
