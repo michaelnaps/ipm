21d145e405e50535fce9b38e3f4b5c5c89983b50
michaelnaps <napoli.38@osu.edu>
2021-10-18 20:27:09 -0400

    began working from mpc pseudocode

--- LIPM_2d_cart/statespace.m
+++ LIPM_2d_cart/statespace.m
@@ -1,6 +1,5 @@
-function dq = mpc_control(q, T)
+function dq = statespace(q, u)
     %% Setup
-    qd = [NaN, 0, pi, 0];  % desired values
     mc = 100;  % [kg]
     mb = 10;   % [kg]
     l = 1;     % [m]
@@ -12,19 +11,6 @@ function dq = mpc_control(q, T)
     q3 = q(3);
     q4 = q(4);
     
-    %% MPC Controller
-    u0 = q(5);  % from previous iteration
-    u_mpc = (u0-10):1:(u0+10);
-    
-    % changing cost function
-    C = @(qc, u) (0-qc(2))^2 + (pi-qc(3))^2 + (0-qc(4))^2 + (u(1)-u(2))^2;
-    
-    % loop for iteration checks
-    for i = 1:length(u_mpc)
-        [t, q_mpc] = ode45(@(t,q) statespace(), 0:1:T, );
-        cost(i) = C(q_mpc, u_mpc);
-    end
-
     %% State Space
     dq = [
          (q2);
@@ -37,3 +23,4 @@ function dq = mpc_control(q, T)
          (u)
          ];
 end
+
