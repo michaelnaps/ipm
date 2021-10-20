a2446d471e9f7ca741c21729a80beda90609206a
michaelnaps <napoli.38@osu.edu>
2021-10-20 18:17:24 -0400

    updated state space...

--- LIPM_2d_cart/MPC/main.m
+++ LIPM_2d_cart/MPC/main.m
@@ -13,35 +13,36 @@ clc;clear;
 close all;
 
 % establish state space vectors
-adj = pi/2;
+um = 500;                 % maximum input change
 dt = 0.1;                 % change in time
-T = 0:dt:10;              % time span
-p = [dt; 2];                    % prediction horizon
+T = 0:dt:30;              % time span
+p = [dt; 9];              % prediction horizon
 s0 = [0; 0];              % cart position and velocity
-th0 = [3*pi/4; 0];        % angular position and velocity
+th0 = [pi; 0];          % angular position and velocity
 q0 = [s0; th0; 0; 0];     % initial state space
 
 % solve for time dependent solution
-[t, q] = ode45(@(t,q) mpc_control(q,p,5000,100,100), T, q0);
+[t, q] = ode45(@(t,q) mpc_control(q,p,um,100,100), T, q0);
 
 % final angle at end of simulation
+disp("Final Input on Cart: " + q(length(q),5) + " [N]")
 disp("Final Velocity of Cart: " + q(length(q),2) + " [m/s]")
 disp("Final Position of Pendulum: " + q(length(q),3) + " [rad]")
 disp("Final Velocity of Pendulum: " + q(length(q),4) + " [rad/s]")
 
-% % velocity and position of cart
-% figure(1)
-% hold on
-% yyaxis left
-% plot(t, q(:,1))
-% ylabel('Position [m]')
-% yyaxis right
-% plot(t, q(:,2))
-% ylabel('Velocity [m/s]')
-% title('Cart Profile')
-% legend('Pos', 'Vel')
-% hold off
-% 
+% velocity and position of cart
+figure(1)
+hold on
+yyaxis left
+plot(t, q(:,1))
+ylabel('Position [m]')
+yyaxis right
+plot(t, q(:,2))
+ylabel('Velocity [m/s]')
+title('Cart Profile')
+legend('Pos', 'Vel')
+hold off
+
 % angular position and velocity of pendulum
 figure(2)
 hold on
@@ -54,44 +55,29 @@ ylabel('Vel [rad/s]')
 title('Angular Profile')
 legend('Angular Position', 'Angular Velocity') 
 hold off
-%  
-% % proportional gain vs. angular position
-% figure(3)
-% hold on
-% plot(t, q(:,6))
-% title('Proportional Gain Plot')
-% ylabel('Kp')
-% xlabel('Time [s]')
-% hold off
-%   
-% % integral gain vs. angular position
-% figure(4)
-% hold on
-% plot(t, q(:,7))
-% title('Integral Gain Plot')
-% ylabel('Ki')
-% xlabel('Time [s]')
-% hold off
 
-% % plot cost from mpc controller
-% figure(5)
-% hold on
-% plot(t, q(:,6))
-% title('MPC Cost Trend')
-% ylabel('Cost')
-% xlabel('Time')
-% hold off
+% plot cost from mpc controller
+figure(3)
+hold on
+plot(t, q(:,6))
+title('MPC Cost Trend')
+ylabel('Cost')
+xlabel('Time')
+hold off
 
 % plot input variable
-figure(8)
+figure(4)
 hold on
 plot(t, q(:,5))
+% plot(t, q(:,5)-um)
+% plot(t, q(:,5)+um)
 title('Input')
-ylabel('Magnitude of Input')
+ylabel('Input vs. Time')
 xlabel('Time')
 hold off
 
 % % animate link motion
+% adj = pi/2;
 % n = length(q(:,1));
 % animation([q(:,3)-adj, zeros(n,1),...
-%              q(:,4)-adj, zeros(n,1)]', T(2)-T(1));
+%              q(:,4)-adj, zeros(n,1)]', dt);
