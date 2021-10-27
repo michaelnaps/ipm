b8c2b3eb12430c823030f518d098da28ffa15fa0
michaelnaps <napoli.38@osu.edu>
2021-10-27 11:36:08 -0400

    working through the optimization problem, pseudocode needed

--- LIPM_2d_cart/MPC_opt/mpc_control.m
+++ LIPM_2d_cart/MPC_opt/mpc_control.m
@@ -1,30 +1,10 @@
 function [T, q] = mpc_control(q0, T, P, um, c1, c2, C)
     %% MPC Controller
     dt = T(2) - T(1);
-    up = [0; linspace(-um,um,1000)'];  % inputs to consider
+    up = [-um; um];  % input constraints
     q = NaN(length(T), length(q0));
     q(1,:) = q0;
-    for i = 2:length(T)
-        
-        % MPC Loop
-        c = Inf;  % reset every loop
-        for j = 1:length(up)
-            % solve for states at next state inputs
-            [~, qc] = ode45(@(t,qc) statespace(qc,up(j),c1,c2),...
-                0:dt:P*dt, q(i-1,1:4)');
-
-            % calculate cost
-            for k = 1:P
-                Cp = sum(C(qc(P+1,:)));
-            end
-            
-            % compare new costs
-            if (Cp < c)
-                c = Cp;
-                % add optimized cost-state to output matrix
-                q(i,:) = [qc(2,:)'; q(i-1,6); up(j); c]; 
-            end
-        end % end of MPC
-        
-    end
+    
+    fmincon(@(q) cost(q), 0, [], [], [], [], [], [],...
+        @(u) statespace(q,u,c1,c2))
 end
