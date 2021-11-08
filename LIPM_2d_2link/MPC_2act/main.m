eec4e05d007e3590732c5617ef3b64396b1fc68c
michaelnaps <napoli.38@osu.edu>
2021-11-07 21:28:40 -0500

    converted inputs and cost to array and cell array

--- LIPM_2d_2link/MPC_2act/main.m
+++ LIPM_2d_2link/MPC_2act/main.m
@@ -21,7 +21,7 @@ T = 0:dt:10;                % time span
 th1_0 = [pi; 1.5];          % cart position and velocity
 th2_0 = [0; 0.0];           % angular position and velocity
 q0 = [th1_0;th2_0;0;0;0];     % initial state space
-um = 1000;                  % maximum input change
+um = [1000; 1000];                  % maximum input change
 
 % Damping Coefficients
 % (interesting behavior when c1 < 20)
@@ -33,7 +33,10 @@ c2 = c1;
 
 %% Cost Function
 %           ang pos.        ang. vel.        cart pos.
-Cq = @(qc) (pi-qc(1)).^2; % + (0-qc(4)).^2; % + (pd-qc(1))^2;
+Cq = {
+    @(qc) 100*(cos(pi)-cos(qc(1))).^2; % + (0-qc(4)).^2; % + (pd-qc(1))^2;
+    @(qc) 100*(cos(pi)-cos(pc(3))).^2;
+     };
 
 %% Implementation
 % solve for time dependent solution
--- LIPM_2d_2link/MPC_2act/statespace.m
+++ LIPM_2d_2link/MPC_2act/statespace.m
@@ -4,6 +4,9 @@ function dq = statespace(q, u, c1, c2)
     m1 = 10;  m2 = 10;      % [kg]
     g = 9.81;               % [m/s^2]
     
+    u1 = u(1);
+    u2 = u(2);
+    
     q1 = q(1);
     q2 = q(2);
     q3 = q(3);
@@ -14,10 +17,10 @@ function dq = statespace(q, u, c1, c2)
     
     dq = [
          (q2);
-         (u - c1*q2*l1 - g*(2*m1 + m2) * sin(q1) - m2*g*sin(q1-2*q3)...
+         (u1 - c1*q2*l1 - g*(2*m1 + m2) * sin(q1) - m2*g*sin(q1-2*q3)...
           - 2*sin(q1-q3)*m2*(q4^2*l2 + q2^2*l1*cos(q1-q3))) / den;
          (q4);
-         (-c2*q4 + 2*sin(q1-q3)*(q2^2*l1*(m1 + m2) + g*(m1 + m2)*cos(q1)...
+         (u2 - c2*q4 + 2*sin(q1-q3)*(q2^2*l1*(m1 + m2) + g*(m1 + m2)*cos(q1)...
           + q4^2*l2*m2*cos(q1 - q3))) / den;
          ];
 end
