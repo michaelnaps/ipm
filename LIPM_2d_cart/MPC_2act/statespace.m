26fbf50b266214851ad5029648fe2369022a2cae
michaelnaps <napoli.38@osu.edu>
2021-11-08 21:04:23 -0500

    updated statespace for 2 inputs

--- LIPM_2d_cart/MPC_2act/statespace.m
+++ LIPM_2d_cart/MPC_2act/statespace.m
@@ -1,27 +1,28 @@
 function dq = statespace(q, u, c1, c2)
     %% Setup
-    l1 = 1;   l2 = 1;       % [m]
-    m1 = 10;  m2 = 10;      % [kg]
-    g = 9.81;               % [m/s^2]
+    % Constants
+    mc = 100;  % [kg]
+    mb = 10;   % [kg]
+    l = 1;     % [m]
+    g = 9.81;  % [m/s^2]
     
-    u1 = u(1);
-    u2 = u(2);
+    u1 = u(1);  % input to cart
+    u2 = u(2);  % input to pendulum
     
-    q1 = q(1);
+    % Current State Values
+    q1 = q(1);   % not needed for computations
     q2 = q(2);
     q3 = q(3);
     q4 = q(4);
     
     %% State Space
-    den = l1*(2*m1 + m2 - 2*cos(2*q1 - 2*q3));
-    
     dq = [
          (q2);
-         (u1 - c1*q2*l1 - g*(2*m1 + m2) * sin(q1) - m2*g*sin(q1-2*q3)...
-          - 2*sin(q1-q3)*m2*(q4^2*l2 + q2^2*l1*cos(q1-q3))) / den;
+         (u1 - c1 * q2 + mb * sin(q3) * (l * q4^2 - g * cos(q3)))...
+               / (mc + mb * sin(q3)^2);               
          (q4);
-         (u2 - c2*q4 + 2*sin(q1-q3)*(q2^2*l1*(m1 + m2) + g*(m1 + m2)*cos(q1)...
-          + q4^2*l2*m2*cos(q1 - q3))) / den;
+         (u2 - u1 * cos(q3) - mb * l * sin(q3) * cos(q3)...
+               - (mc + mb) * g * sin(q3) - c2 * l * q4)...
+               / (l * (mc + mb * sin(q3)^2));
          ];
 end
-
