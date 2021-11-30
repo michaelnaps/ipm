0df39947a79f9c2c31cf5f999ed10bcfbd841c58
Michael Napoli <napoli.38@osu.edu>
2021-11-30 14:42:28 -0500

    converted to more efficient cost and jacobian function format

--- LIPM_2d_3link/gaussnewton/cost.m
+++ LIPM_2d_3link/gaussnewton/cost.m
@@ -4,15 +4,10 @@ function [C, J] = cost(P, dt, q0, u, c, m, L, Cq, Jq)
     [~, qc] = ode45(@(t,q) statespace(q, u, c, m, L), 0:dt:P*dt, q0);
     
     % sum of cost over the prediction horizon states
-    C = zeros(size(Cq));
-	J = zeros(size(Jq));
-    for i = 1:length(Cq)
-        for k = 1:P+1
-            C(i) = C(i) + Cq{i}(qc(k,:));
-            
-            for j = 1:length(Jq(1,:))
-                J(i,j) = J(i,j) + Jq{i,j}(qc(k,:));
-            end
-        end
+    C = zeros(size(u));
+	J = zeros(length(u));
+    for i = 1:P+1
+        C = C + Cq(qc(i,:));
+        J = J + Jq(qc(i,:));
     end
 end
