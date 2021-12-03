01a73a53d2cb71f0d4d2b26f538ccc4ec369c816
michaelnaps <napoli.38@osu.edu>
2021-12-03 17:41:57 -0500

    created ode comparison folder

---
+++ LIPM_2d_3link/00_ode_testing/main.m
@@ -0,0 +1,24 @@
+%% Environment Setup
+clc;clear;
+close all;
+
+restoredefaultpath
+addpath ../.
+
+%% Variable Setup
+T = 0:0.025:20;
+
+c = [500; 500; 500];            % damping coefficients
+m = [15; 15; 60];
+L = [0.5; 0.5; 1];
+
+th1_0 = [pi/2;0.0];             % link 1 position and velocity
+th2_0 = [0.0; 2.0];             % link 2 position and velocity
+th3_0 = [0.0; 0.0];             % link 3 position and velocity
+
+q0 = [th1_0;th2_0;th3_0];
+
+%% ODE Comparison Functions
+% statespace(q, u, c, m, L)
+[~,q_ode45] = ode45(@(t,q) statespace(q,[0;0;0],c,m,L), T, q0);
+q_euler = ode_euler(
