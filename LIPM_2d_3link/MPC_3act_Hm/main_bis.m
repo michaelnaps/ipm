7d0bbde566541f90bcd9369937415382476ae8fe
Michael Napoli <napoli.38@osu.edu>
2021-12-03 11:07:02 -0500

    added runtime output

--- LIPM_2d_3link/MPC_3act_Hm/main_bis.m
+++ LIPM_2d_3link/MPC_3act_Hm/main_bis.m
@@ -32,7 +32,7 @@ P = 4;                          % prediction horizon [time steps]
 dt = 0.025;                     % change in time
 T = 0:dt:10;                    % time span
 th1_0 = [pi/2;0.0];             % link 1 position and velocity
-th2_0 = [0.0; 5.0];             % link 2 position and velocity
+th2_0 = [0.0; 2.0];             % link 2 position and velocity
 th3_0 = [0.0; 0.0];             % link 3 position and velocity
 um = [3000; 2000; 1500];        % maximum input to joints
 c = [500; 500; 500];            % damping coefficients
@@ -54,6 +54,7 @@ q0 = [
 CoM = map_CoM(q, m, L);
 
 %% Graphing and Evaluation
+fprintf("Total Runtime: -------------------- %.4f [s]\n", sum(q(:,14)))
 fprintf("Final Input at Link 1 ------------- %.4f [Nm]\n", q(length(q),7))
 fprintf("Final Input at Link 2 ------------- %.4f [Nm]\n", q(length(q),8))
 fprintf("Final Input at Link 3 ------------- %.4f [Nm]\n", q(length(q),9))
