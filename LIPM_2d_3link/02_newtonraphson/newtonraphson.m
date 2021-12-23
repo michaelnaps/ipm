a53dd6a9f2baff370f7881dc270ce99ad084b129
Michael Napoli <napoli.38@osu.edu>
2021-12-22 22:52:57 -0500

    rewrote the nnr method, change in input on each iteration too small

--- LIPM_2d_3link/02_newtonraphson/newtonraphson.m
+++ LIPM_2d_3link/02_newtonraphson/newtonraphson.m
@@ -1,65 +1,53 @@
-function [u, C, n] = newtonraphson(P, dt, q0, u0, um, c, m, L, Cq, eps)
-    %% Gauss Newton Method to Solve for Next Input
-    %  notation: subscript 'c' - current
-    %            subscript 'n' - next
-
-    % constraints
-    umin = -um;
-    umax = um;
-    
-    % initial guess is set to previous input
+function [u, C, n, brk] = newtonraphson(P, dt, q0, u0, um, c, m, L, Cq, eps)
+    %% Setup
     a = 1;
-    brk = 0;
+    N = length(um);
     uc = u0;
-    Cc = cost(P, dt, q0, uc, c, m, L, Cq, 'Initial Cost Calculation');
-    Jc = cost_jacobian(P, dt, q0, uc, c, m, L, Cc, Cq);
-    un = uc;  Cn = Cc;
+    Cc = cost(P, dt, q0, uc, c, m, L, Cq, 'NNR Initial Cost');
+    Jc = cost_gradient(P, dt, q0, uc, c, m, L, Cq, eps);
 
     count = 1;
+    brk = 0;
     while (Cc > eps)
-        udn = a*(Jc/Cc)
-        un = uc - udn;
-
-        for i = 1:length(uc)
-            if (un(i) > umax(i))
-                un(i) = umax(i);
-            elseif (un(i) < umin(i))
-                un(i) = umin(i);
+        lambda = Jc/Cc;
+        un = uc - a*lambda;
+        
+        % check boundary constraints
+        for i = 1:N
+            if (un(i) > um(i))
+                un(i) = um(i);
+            elseif (un(i) < -um(i))
+                un(i) = -um(i);
             end
         end
-        
-        Cn = cost(P, dt, q0, un, c, m, L, Cq, "Optimization Loop (iter: " + count + ")");
-        Jn = cost_jacobian(P, dt, q0, un, c, m, L, Cn, Cq);
-        
+
+        Cn = cost(P, dt, q0, un, c, m, L, Cq, 'NNR Initial Cost');
+        Jn = cost_gradient(P, dt, q0, un, c, m, L, Cq, eps);
+
         Cdn = abs(Cn - Cc);
         count = count + 1;
-        
-        if (sum(udn < eps) == length(udn))
-            brk = 1;
-            break;
-        end
-        
+
         if (Cdn < eps)
-            brk = 2;
+            fprintf("Change in cost break.\n")
+            brk = 1;
             break;
         end
 
         if (count == 1000)
+            fprintf("ERROR: Iteration break (1000).\n")
             brk = -1;
             break;
         end
 
         uc = un;  Cc = Cn;  Jc = Jn;
     end
-    
-    % iteration break
-    if (count == 1000)
-        fprintf("ERROR: Optimization exited - 1000 iterations reached.\n")
-    end
 
-    fprintf("Input found: u1 = %.3f  u2 = %.3f  u3 = %.3f  (%i)  (%i)\n", un, count, brk)
+    if (brk == 0)
+        fprintf("Zero cost break.\n")
+    end
 
     u = un;
     C = Cn;
     n = count;
 end
+
--- LIPM_2d_3link/MPC_3act_Hm/main_nnr.m
+++ LIPM_2d_3link/MPC_3act_Hm/main_nnr.m
@@ -32,7 +32,7 @@ L = [0.5; 0.5; 1];
 % establish state space vectors and variables
 P = 4;                          % prediction horizon [time steps]
 dt = 0.025;                     % change in time
-T = 0:dt:10;                    % time span
+T = 0:dt:2;                    % time span
 th1_0 = [pi/2;0.0];             % link 1 position and velocity
 th2_0 = [0.0; 0.0];             % link 2 position and velocity
 th3_0 = [0.0; 0.0];             % link 3 position and velocity
