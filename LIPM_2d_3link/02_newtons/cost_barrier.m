7f0f98daad6cf6e3f5ec630adf9e5d2ba4569537
Michael Napoli <napoli.38@osu.edu>
2021-12-28 18:46:08 -0500

    abs taken and new data created

--- LIPM_2d_3link/02_newtons/cost_barrier.m
+++ LIPM_2d_3link/02_newtons/cost_barrier.m
@@ -2,11 +2,11 @@ function [Cb] = cost_barrier(q, mu)
     wmax = 3;
 
     Cineq = [
-         (wmax - q(2));
-         (wmax - q(4));
-         (wmax - q(6));
+         abs(wmax - q(2));
+         abs(wmax - q(4));
+         abs(wmax - q(6));
         ];
 
-    Cb = mu * (-log(Cineq));
+    Cb = mu*(-log(Cineq));
 end
 
